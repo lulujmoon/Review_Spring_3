ed8c3c9a686e27911a57045d4909fbb73e8612a2
lulujmoon <80494527+lulujmoon@users.noreply.github.com>
2021-03-28 17:23:24 +0900

    0328

--- src/main/java/com/redbeet/whatI/member/MemberDAO.java
+++ src/main/java/com/redbeet/whatI/member/MemberDAO.java
@@ -1,8 +1,18 @@
 package com.redbeet.whatI.member;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class MemberDAO {
+	
+	@Autowired
+	private SqlSession sqlSession;
 
+	private final String NAMESPACE = "com.redbeet.whatI.member.MemberDAO.";
+	
+	public MemberDTO memberLogin(MemberDTO memberDTO) throws Exception {
+		return sqlSession.selectOne(NAMESPACE+"memberLogin", memberDTO);
+	}
 }
